cf0e151700cddfaaeac748682c9a843ec026a447
Bui Trung Khang <khang.bui@dnse.com.vn>
2025-03-24 10:49:25 +0700

    fix: add isPaused = false to start function

--- android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
+++ android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
@@ -358,6 +358,7 @@ class MobileScanner(
                     analysis
                 )
                 cameraSelector = cameraPosition
+                isPaused = false
             } catch(exception: Exception) {
                 mobileScannerErrorCallback(NoCamera())
 
